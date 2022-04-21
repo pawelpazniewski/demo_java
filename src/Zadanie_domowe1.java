7ff59c0ed5ea53acaa18a88a357490102b60952a
Pawel <pawelpazniewski@hotmail.com>
2022-04-21 12:16:25 +0200

    Pierwsze zadanie domowe

--- src/Zadanie_domowe1.java
+++ src/Zadanie_domowe1.java
@@ -1,5 +1,27 @@
 public class Zadanie_domowe1 {
     public static void main(String[] args) {
-        System.out.println("Jestem w zadaniu domowym");
+        int num1 = 6456;
+        int num2 = 54342;
+        int num3 = 123;
+        int num4 = 78978;
+
+        int sum12 = num1 + num2;
+        int sum34 = num3 + num4;
+//        int sumBoth = sum12 + sum34;
+
+        if (sum12 % 2 == 0) {
+            System.out.println("Suma dwóch pierwszych jest parzysta");
+        }
+        if (sum34 % 2 == 0) {
+            System.out.println("Suma dwóch ostatnich jest parzysta");
+        } else {
+            System.out.println("Suma dwóch ostatnich jest nieparzysta");
+        }
+        if (sum12 % 2 ==0 && sum34 % 2 ==0){
+            System.out.println("Obie sumy są parzyste");
+
+        }else{
+            System.out.println("Obie sumy nie są parzyste");
+        }
     }
 }
